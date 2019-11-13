ae169d9d63a981e213e1a59e7af1b082fef49ecf
nabinpoudyal3 <fs2973@wayne.edu>
2019-11-13 15:20:15 -0600

    script for extraction of misIDEleSF

---
+++ runAllFitting.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+echo "Running makeAllZJets Prefit for all 3 years"
+./makeAllZJetsPrefit.sh
+echo "Now Run combine Jupyter and get ZJetsSF for all 3 years!!"
+while [ true ] ; do
+	read -t 60 -n 1
+	if [ $? = 0 ] ; then
+	echo "got all Zjets SFs"
+	break;
+	else
+	echo "waiting to finish extraction of ZJetsSF."
+	fi
+done
+
+echo "Now running makeAll4template Prefit for all 3 years"
+./makeAll4templatePrefit.sh
+echo "Now running makeAllYearPrefitPdf for pdf "
+./makeAllYearPrefitPdf.sh
+
+echo "Now Run combine Jupyter and get MisIDEleSF for all 3 years!!"
+while [ true ] ; do
+	read -t 60 -n 1
+	if [ $? = 0 ] ; then
+	echo "got all MisIDEleSFs"
+	break;	
+	else
+	echo "waiting to finish extraction of MisIDEleSF."
+	fi
+done
+
+echo "Now runing  all postfit plots "
+./makeAllZJetsPostfit.sh
+./makeAll4templatePostfit.sh
+echo "Copy Pre post plots and tex files to make slides"
